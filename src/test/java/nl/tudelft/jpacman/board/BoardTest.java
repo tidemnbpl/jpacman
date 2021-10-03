19b90feb4ff91277f485d01140ec16114f0334c6
tidemnbpl <rungsimun.man@student.mahidol.edu>
2021-10-04 05:47:12 +0700

    To increase the coverage test

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -1,48 +1,119 @@
 package nl.tudelft.jpacman.board;
 
-import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestFactory;
-
-import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 
+/**
+ * Board class test, simple one to test the class.
+ */
+public class BoardTest {
 
-class BoardTest{
-    private static final int MAX_WIDTH = 2;
-    private static final int MAX_HEIGHT = 3;
-
-    private final Square[][] grid = {
-        {mock(Square.class), mock(Square.class), mock(Square.class)},
-        {mock(Square.class), mock(Square.class), mock(Square.class)}
-    };
-    private final Board board = new Board(grid);
-
+    /**
+     * Is the board valid?
+     */
     @Test
-    void verifyWidth(){
-        assertThat(board.getWidth()).isEqualTo(MAX_WIDTH);
+    void testValid() {
+        Square[][] grid = {
+            { mock(BasicSquare.class) },
+            { mock(BasicSquare.class) }
+        };
+        Board board = new Board(grid);
+        assertThat(board.invariant()).isEqualTo(true);
     }
+
+    /**
+     * Is the squareat?
+     */
     @Test
-    void verifyHeight(){
-        assertThat(board.getHeight()).isEqualTo(MAX_HEIGHT);
+    void testSquareAtNull() {
+        Square[][] grid = {
+            { mock(BasicSquare.class) },
+            { null }
+        };
+        Board board = new Board(grid);
+        assertThat(board.squareAt(0, 0)).isEqualTo(grid[0][0]);
+        assertThat(board.squareAt(1, 0)).isNotEqualTo(grid[0][0]);
+        assertThat(board.squareAt(0, 0)).isNotEqualTo(grid[1][0]);
+
+    }
+    /**
+     * Is the squareat?
+     */
+    @Test
+    void testSquareAt() {
+        Square[][] grid = {
+            { mock(BasicSquare.class) },
+            { mock(BasicSquare.class) }
+        };
+        Board board = new Board(grid);
+        assertThat(board.squareAt(0, 0)).isEqualTo(grid[0][0]);
+        assertThat(board.squareAt(1, 0)).isNotEqualTo(grid[0][0]);
+        assertThat(board.squareAt(0, 0)).isNotEqualTo(grid[1][0]);
+
     }
 
-    @TestFactory
-    Iterable<DynamicTest> verifySquares(){
-        return Arrays.asList(
-            testSquareAt(0,0),
-            testSquareAt(1,1),
-            testSquareAt(0,2)
-        );
+
+        @Test
+        void test() {
+
+
+            assertThrows(AssertionError.class, () -> {
+                new Board(null);
+            });
+
+        }
+
+    /**
+     * Is square within the border?
+     */
+    @Test
+    void testWithinBorders() {
+        Square[][] grid = {
+            { mock(BasicSquare.class) },
+            { mock(BasicSquare.class) }
+        };
+        Board board = new Board(grid);
+        // boundary getWidth() = 2 (x), getHeight() = 1 (y)
+        assertThat(board.withinBorders(0, 0)).isTrue(); // x in range, y in range
+        assertThat(board.withinBorders(15, 15)).isFalse(); // x > getWidth(), y > getHeight()
+        assertThat(board.withinBorders(-1, -1)).isFalse(); // x < 0, y < 0
+        assertThat(board.withinBorders(-1, 1)).isFalse(); // x < 0, y in range
+        assertThat(board.withinBorders(0, -1)).isFalse(); // x in range, y < 0
+        assertThat(board.withinBorders(0, 15)).isFalse(); // x in range, y > getHeight()
     }
 
-    private DynamicTest testSquareAt(int x, int y) {
-        return dynamicTest(
-            x + "," + y,
-            () -> assertThat(board.squareAt(x, y)).isEqualTo(grid[x][y])
-        );
+    /**
+     * Is square width of the board correct?
+     */
+    @Test
+    void testGetWidth() {
+        Square[][] grid = {
+            { mock(BasicSquare.class) },
+            { mock(BasicSquare.class) }
+        };
+        Board board = new Board(grid);
+        assertThat(board.getWidth()).isEqualTo(2);
+        assertThat(board.getWidth()).isNotEqualTo(0);
     }
+
+    /**
+     * Is square width of the board correct?
+     */
+    @Test
+    void testGetHeight() {
+        Square[][] grid = {
+            { mock(BasicSquare.class) },
+            { mock(BasicSquare.class) }
+        };
+        Board board = new Board(grid);
+        assertThat(board.getHeight()).isEqualTo(1);
+        assertThat(board.getHeight()).isNotEqualTo(0);
+    }
+
+
+
 }
--- src/test/java/nl/tudelft/jpacman/board/DirectionTest.java
+++ src/test/java/nl/tudelft/jpacman/board/DirectionTest.java
@@ -1,8 +1,8 @@
 package nl.tudelft.jpacman.board;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * A very simple (and not particularly useful)
@@ -20,18 +20,27 @@ public class DirectionTest {
         assertThat(north.getDeltaY()).isEqualTo(-1);
     }
 
+    /**
+     * Do we get the correct delta when moving south?
+     */
     @Test
     void testSouth() {
         Direction south = Direction.valueOf("SOUTH");
         assertThat(south.getDeltaY()).isEqualTo(1);
     }
 
+    /**
+     * Do we get the correct delta when moving west?
+     */
     @Test
     void testWest() {
         Direction west = Direction.valueOf("WEST");
         assertThat(west.getDeltaX()).isEqualTo(-1);
     }
 
+    /**
+     * Do we get the correct delta when moving east?
+     */
     @Test
     void testEast() {
         Direction east = Direction.valueOf("EAST");
