27e2197cac3adffa8c97911c103beed81055da42
Tide Mnbpl <54932935+tidemnbpl@users.noreply.github.com>
2021-11-02 15:37:25 +0700

    Update BoardTest.java

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -1,86 +1,36 @@
 package nl.tudelft.jpacman.board;
 
-
-
-import org.junit.jupiter.api.DynamicTest;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestFactory;
-
-
-
-import java.util.Arrays;
-
-
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 import static org.mockito.Mockito.mock;
-
-
+import org.junit.jupiter.api.Test;
 /**
-* A very simple (and not particularly useful)
-* test class to have a starting point where to put tests.
-*
-* @author Arie van Deursen
-*/
+ * A very simple (and not particularly useful)
+ * test class to have a starting point where to put tests.
+ *
+ * @author Arie van Deursen
+ */
+public class BoardTest {
+    /**
+     * Do we get the correct delta when moving north?
+     */
+    @Test
+    void testboard() {
+        Square[][] oneoneBoard = {
+            { mock(BasicSquare.class) },
+            { mock(BasicSquare.class) }
+        };
+        Board board = new Board(oneoneBoard);
+        assertThat(board.invariant()).isEqualTo(true);
+    }
 
-class BoardTest {
+//    @Test
+//    void squareAt() {
+//        Square[][] oneoneBoard = {
+//            { null },
+//            { mock(BasicSquare.class) }
+//        };
+//        Board board = new Board(oneoneBoard);
+//        assertThat(board.invariant()).isEqualTo(true);
+//    }
 
-
-
-private static final int MAX_WIDTH = 2;
-private static final int MAX_HEIGHT = 3;
-
-
-
-private final Square[][] grid = {
-{ mock(Square.class), mock(Square.class), mock(Square.class) },
-{ mock(Square.class), mock(Square.class), mock(Square.class) },
-};
-private final Board board = new Board(grid);
-
-/**
-* Verifies the board has the correct width.
-*/
-@Test
-void verifyWidth() {
-assertThat(board.getWidth()).isEqualTo(MAX_WIDTH);
-}
-
-
-
-/**
-* Verifies the board has the correct height.
-*/
-@Test
-void verifyHeight() {
-assertThat(board.getHeight()).isEqualTo(MAX_HEIGHT);
-}
-/**
-* Test at various places that the board returns the correct square.
-* @return Tests for the various places.
-*/
-@TestFactory
-Iterable<DynamicTest> verifySquares() {
-return Arrays.asList(
-testSquareAt(0, 0),
-testSquareAt(1, 2),
-testSquareAt(0, 1)
-);
-}
-
-
-
-/**
-* Create a test that board returns correct square.
-* @param x x-coordinate
-* @param y y-coordinate
-* @return Test checking that (x,y) yields correct square.
-*/
-private DynamicTest testSquareAt(int x, int y) {
-return dynamicTest(
-x + "," + y,
-() -> assertThat(board.squareAt(x, y)).isEqualTo(grid[x][y])
-);
-}
 }
